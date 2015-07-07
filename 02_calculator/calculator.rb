c9d528115a4cb14d98855dd3e2e686b0e68af180
Michael <whatahrookie@gmail.com>
2015-07-06 18:09:33 -0700

    all tests including bonus pass

---
+++ 02_calculator/calculator.rb
@@ -0,0 +1,23 @@
+def add(num_one, num_two)
+	num_one + num_two
+end
+
+def subtract(num_one, num_two)
+	num_one - num_two
+end
+
+def sum(array)
+	array.reduce(0, :+)
+end
+
+def multiply(array)
+	array.inject(:*)
+end
+
+def power(num_one, num_two)
+	num_one ** num_two
+end
+
+def factorial(num)
+	(1..(num.zero? ? 1 : num)).inject(:*)
+end
